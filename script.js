498d71c14f6dfdec12bd0029f9f2226a242b4f1b
SamuelInocencio <samuelferreirainocencio@gmail.com>
2025-01-05 13:40:33 -0300

    atualizado projeto

--- script.js
+++ script.js
@@ -1,16 +1,2 @@
-// Função para enviar o formulário
-document.getElementById('contato-form').addEventListener('submit', function (event) {
-    event.preventDefault(); // Impede o envio padrão do formulário
-
-    // Exibe a mensagem de sucesso
-    const mensagemSucesso = document.getElementById('mensagem-sucesso');
-    mensagemSucesso.classList.remove('hidden');
-
-    // Limpa os campos do formulário
-    this.reset();
-
-    // Oculta a mensagem de sucesso após 3 segundos
-    setTimeout(() => {
-        mensagemSucesso.classList.add('hidden');
-    }, 3000);
-});
+// Código JavaScript inicial
+console.log("Bem-vindo ao Projeto em Branco!");
