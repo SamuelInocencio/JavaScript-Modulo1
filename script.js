3f63d3f6e0ff56412005a18d96d0c53e813a089d
SamuelInocencio <samuelferreirainocencio@gmail.com>
2025-01-05 15:56:58 -0300

    atualização da página

--- script.js
+++ script.js
@@ -1,2 +1,55 @@
 // Código JavaScript inicial
-console.log("Bem-vindo ao Projeto em Branco!");
+const pessoas = [{
+        nome: "João",
+        idade: 25,
+        sexo: "Masculino"
+    },
+    {
+        nome: "Maria",
+        idade: 30,
+        sexo: "Feminino"
+    },
+    {
+        nome: "Pedro",
+        idade: 20,
+        sexo: "Masculino"
+    },
+    {
+        nome: "Ana",
+        idade: 28,
+        sexo: "Feminino"
+    },
+    {
+        nome: "Carlos",
+        idade: 35,
+        sexo: "Masculino"
+    },
+    {
+        nome: "Júlia",
+        idade: 22,
+        sexo: "Feminino"
+    },
+    {
+        nome: "Lucas",
+        idade: 27,
+        sexo: "Masculino"
+    },
+    {
+        nome: "Bianca",
+        idade: 24,
+        sexo: "Feminino"
+    },
+    {
+        nome: "Rafael",
+        idade: 29,
+        sexo: "Masculino"
+    },
+    {
+        nome: "Clara",
+        idade: 26,
+        sexo: "Feminino"
+    }
+];
+
+pessoas[0].nome = "Samuel";
+console.log(pessoas);
